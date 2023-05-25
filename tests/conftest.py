ea16bfbaf62c3aaf7e70990966e04e9a00d5a441
Natia Chikovani <124543376+NatiChiko@users.noreply.github.com>
2023-05-25 11:51:55 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,7 +25,7 @@ def setup(request):
         service_obj = Service("C:\\ProgramData\\chocolatey\\bin\\geckodriver.exe")
         driver = webdriver.Firefox(service=service_obj)
     driver.implicitly_wait(15)
-    driver.get("http://shopping.beeyor.com/")
+    driver.get("https://www.tdameritrade.com/tools-and-platforms/thinkorswim.html?TID=NA&ef_id=Cj0KCQjwjryjBhD0ARIsAMLvnF_-lfstevKaaZSZ6AuokimdFFNZ8SMoYueo_Q75MAfMEJoGngyIyKQaAuHTEALw_wcB:G:s&s_kwcid=AL!2521!3!516515983854!e!!g!!think%20or%20swim&CID=PSTOS&gad=1&dclid=CjgKEAjwjryjBhCO6t3GjrXByzcSJADfpqL9qkXUMfA91kEbJVqsEhZRf8MpkSQrvUjXeixlCQKPJ_D_BwE")
     request.cls.driver = driver
     yield
     driver.quit()
